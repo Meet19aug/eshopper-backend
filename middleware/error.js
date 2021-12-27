30a87b95d494da2d8296368a2be1c96795b7acb7
Meet Patel <meetpatel19aug@gmail.com>
2021-12-27 22:30:11 +0530

    Edited Error.js file to handle specific type of error & Authentication is Completed.

--- middleware/error.js
+++ middleware/error.js
@@ -10,6 +10,27 @@ module.exports = (err,req,res,next)=>{
         const message = `Resource not found, Invalid : ${err.path}`;
         err = new ErrorHandler(message, 400);
     }
+
+    // Mongoose Duplicate key Error:
+    //"message": "E11000 duplicate key error collection: Eshopper.users index: email_1 dup key: { email: \"meet007@gmail.com\" }"
+    if(err.code === 11000){
+        const message = `Duplicate ${Object.keys(err.keyValue)} entered`
+        err = new ErrorHandler(message, 400);
+
+        
+    }
+
+    //Json Web Token Error
+    if(err.name === "JsonWebTokenError"){
+        const message = `Json Web Token is Invalid, Try again `;
+        err = new ErrorHandler(message, 400);
+    }
+
+    //JWT Expire Error
+    if(err.name === "TokenExpiredError"){
+        const message = `Json Web Token is Expired, Try again `;
+        err = new ErrorHandler(message, 400);
+    }
     
     res.status(err.statusCode).json({
         success:false,
