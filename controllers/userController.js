363257a032543bfb188fb32829a49029f3ec4f1a
Meet Patel <meetpatel19aug@gmail.com>
2022-01-18 17:22:40 +0530

    change the resetPassword URL to check reset password working locally on frontend port.

--- controllers/userController.js
+++ controllers/userController.js
@@ -83,7 +83,9 @@ exports.forgotPassword = catchAsyncError(async (req, res, next) => {
     //To save reset token temporary in the database.
     await user.save({ validateBeforeSave: false });
 
-    const resetPasswordUrl = `${req.protocol}://${req.get("host")}/api/v1/password/reset/${resetToken}`;
+    // const resetPasswordUrl = `${req.protocol}://${req.get("host")}/api/v1/password/reset/${resetToken}`;
+    const resetPasswordUrl = `${process.env.FRONTEND_URL}/password/reset/${resetToken}`;
+
 
     const message = `Your password reset token is :- \n\n ${resetPasswordUrl} \n\n If you have not requested this email than please ignore it.`
 
