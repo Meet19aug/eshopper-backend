2e8ad33788cebaad3a757d0bbcb068d481500e94
Meet Patel <meetpatel19aug@gmail.com>
2022-01-18 10:28:34 +0530

    Added Code For Clodinary for Update Profile in userController.

--- models/userModel.js
+++ models/userModel.js
@@ -39,6 +39,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         default: "user",
     },
+    createdAt:{
+        type: Date,
+        default: Date.now,
+    },
     resetPasswordToken: String,
     resetPasswordExpire: Date,
 
