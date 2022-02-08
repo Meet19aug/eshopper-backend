391811ab03ffcc3df507a72f3ad8038e5cea7739
Meet Patel <meetpatel19aug@gmail.com>
2022-02-08 23:01:20 +0530

    Modified Code for Delete Review to manage proper ratings.

--- controllers/productController.js
+++ controllers/productController.js
@@ -224,8 +224,12 @@ exports.deleteReview = catchAsyncError(async (req, res, next) => {
         avg += rev.rating;
     });
 
-    const ratings = avg / reviews.length;
-
+    let ratings=0;
+    if(reviews.length===0){
+        ratings=0;
+    }else{
+        ratings = avg / reviews.length;
+    }
     const numOfReviews = reviews.length;
 
     await Product.findByIdAndUpdate(
